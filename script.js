e79b5cc49898710a5e2e71ed84ae5bf4c07861cc
zehra <71967067+zehraemul@users.noreply.github.com>
2022-11-10 01:36:08 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var loader = document.querySelector(".loader");
+var content = document.querySelector(".content");
+
+window.addEventListener("load",(()=>{
+    loader.style.display ="none";
+    content.style.display ="block"
+}))
